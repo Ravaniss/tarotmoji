93a704dec86743750c7cda80f39810d9f5c14ee9
Ravaniss <senechal.max06@gmail.com>
2019-08-26 21:28:28 +0200

    VoiceRecorder - Record / Play record OK - List of Record - Problem on playing the saving record

---
+++ app/api/sounds.js
@@ -0,0 +1,31 @@
+const appSettings = require("application-settings");
+
+
+const sounds = {
+  getSounds() {
+    let json = appSettings.getString('sounds', '');
+    if(json === '') return [];
+    else return JSON.parse(json);
+  },
+  removeSound(fileName) {
+    let sounds = this.getSounds();
+    sounds = sounds.filter(s => {
+      return s.fileName != fileName;
+    });
+    //serialize it
+    let json = JSON.stringify(sounds);
+    appSettings.setString('sounds', json);
+  },
+  saveSound(name, fileName) {
+    let sounds = this.getSounds();
+    sounds.push({
+      name:name,
+      fileName:fileName
+    });
+    //serialize it
+    let json = JSON.stringify(sounds);
+    appSettings.setString('sounds', json);
+  }
+}
+
+module.exports = sounds;
---
+++ app/views/ListOfRecords.vue
@@ -0,0 +1,81 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar">
+      <Label class="action-bar-title" text="Records"></Label>
+    </ActionBar>
+
+    <StackLayout>
+      <Label v-if="sounds.length === 0" text="You haven't recorded any sounds yet." />
+
+      <ListView for="sound in sounds" @tap="playSound">
+        <v-template>
+          <Label :text="sound.name" :filename="sound.fileName" @longPress="deleteSound" />
+        </v-template>
+      </ListView>
+      <Button text="Record New Sounds" @tap="goToRecord" />
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+  const audio = require('nativescript-audio');
+  const fileSystemModule = require('tns-core-modules/file-system');
+  import soundsAPI from '../api/sounds';
+  import VoiceRecorder from './VoiceRecorder';
+
+  export default {
+    data() {
+      return {
+        audioFolder:null,
+        sounds:[]
+      }
+    },
+    created() {
+      this.sounds = soundsAPI.getSounds();
+      this.audioFolder = fileSystemModule.knownFolders.currentApp().getFolder('recordings');
+    },
+    methods:{
+      async deleteSound(event) {
+        let filename = event.object.filename;
+        let confirmOptions = {
+          title: "Delete Sound",
+          message: "Do you want to delete this sound?",
+          okButtonText: "Yes",
+          cancelButtonText: "No"
+        };
+        confirm(confirmOptions).then(async result => {
+          if(result) {
+            // first delete the file
+            let file = this.audioFolder.getFile(filename);
+            console.log('going to try to delete', file);
+            await file.remove();
+            soundsAPI.removeSound(filename);
+            this.sounds = soundsAPI.getSounds();
+          }
+        });
+      },
+      goToRecord() {
+        this.$navigateTo(VoiceRecorder, {frame: 'mainContent'});
+      },
+      async playSound(event) {
+        console.log(event.item.fileName)
+        /*console.log('play sound tapped', event.item.fileName);
+        let player = new audio.TNSPlayer();
+        await player.playFromFile({
+          audioFile:this.audioFolder.path+'/'+event.item.fileName
+        });*/
+      }
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+  // Start custom common variables
+  // End custom common variables
+  StackLayout {
+    padding: 10px;
+  }
+  Label {
+    padding: 20px;
+  }
+</style>
