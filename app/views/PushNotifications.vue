fcd679d782486e512df4f08206e4807ff4619d33
Ravaniss <senechal.max06@gmail.com>
2019-08-29 02:32:30 +0200

    Creation of pushnotifications file

---
+++ app/views/PushNotifications.vue
@@ -0,0 +1,33 @@
+<template>
+  <Page>
+    <ActionBar title="Push Notif"/>
+    <Label text="Hello world !"></Label>
+  </Page>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  ActionBar {
+    background-color: #53ba82;
+    color: #ffffff;
+  }
+  .message {
+    font-size: 17;
+    margin: 17;
+    color: #53ba82;
+  }
+  Button.btn {
+    font-size: 14;
+    border-radius: 3;
+    border-width: 2;
+    border-color: #63d4a5;
+    color: #63d4a5;
+    padding: 12;
+    margin: 16;
+  }
+</style>
